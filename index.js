ad1c216f65b0f896ca73a7a74b313d28065dd299
PabloPianelo <pablopianeloxd@gmail.com>
2025-04-22 09:42:42 +0200

    implemetacion de morgan, helmet y express validator

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
+
 // Middleware para procesar JSON y datos de formularios
 app.use(cors()); //cors
+// middleware que añade cabeceras extra de seguridad a las respuestas
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//mas seguridad al servidor
+app.use(helmet());
+//para ver las peticiones
+app.use(morgan('tiny'));
+
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI)
---
+++ middlewares/validationChecker.js
@@ -0,0 +1,14 @@
+const { validationResult } = require("express-validator");
+
+// middleware que lee los errores y los devuelve en la respuesta, si los hubiera
+const validationChecker = (req, res, next) => {
+  const result = validationResult(req);
+  const errors = result.array();
+  if (errors.length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  next();
+};
+
+module.exports = validationChecker;
